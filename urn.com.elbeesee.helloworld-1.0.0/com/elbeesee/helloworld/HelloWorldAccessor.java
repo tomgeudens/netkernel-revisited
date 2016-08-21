fa25ef1d216942c85269e96020d254646036d811
tomgeudens <tom.geudens@hushmail.com>
2016-08-21 17:33:44 +0200

    added urn.com.elbeesee.helloworld-1.0.0

---
+++ urn.com.elbeesee.helloworld-1.0.0/com/elbeesee/helloworld/HelloWorldAccessor.java
@@ -0,0 +1,35 @@
+package com.elbeesee.helloworld;
+
+/**
+ * 
+ * Elephant Bird Consulting
+ * 
+ * @author tomgeudens
+ *
+ */
+
+/**
+ * Accessor Imports
+ */
+import org.netkernel.layer0.nkf.*;
+//import org.netkernel.layer0.meta.impl.SourcedArgumentMetaImpl;
+import org.netkernel.module.standard.endpoint.StandardAccessorImpl;
+
+/**
+ * Processing Imports
+ */
+
+/**
+ * Hello World Accessor
+ */
+
+public class HelloWorldAccessor extends StandardAccessorImpl {
+	public HelloWorldAccessor() {
+		this.declareThreadSafe();
+		this.declareSourceRepresentation(String.class);
+	}
+	
+	public void onSource(INKFRequestContext aContext) throws Exception {
+		aContext.createResponseFrom("Hello World !");
+	}
+}
